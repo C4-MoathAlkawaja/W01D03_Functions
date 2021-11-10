f9af14b91ef53524a082000f6eae72dac442370c
MoathAlkawaja <93875570+MoathAlkawaja@users.noreply.github.com>
2021-11-10 18:39:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log(arrowTitle)
+/* START CODE UNDER THIS LINE */
+
+//Pulse Check 
+
+//  Q1)Figure out the syntax errors in the following functions, and fix them.
+
+const myFunction = function parameter) {} //function(parameter)
+const my Second Function = function (parameter) {} //const mySecondFunction = function(parameter) {}
+const function = function (parameter) {} //constfunction = function(parameter) {}
+const functionName = function (paramOne paramTwo) {} // const functionName = function (paramOne,paramTwo) {}
+
+//  Q2)Write a function sayHello that returns Hello when executed.
+
+const sayHello = function (Hello) {
+    return sayHello
+  };
+  
+//  Q3) Write a function myAge that accepts one argument age and returns the age when executed.
+
+const myAge = function (age) {
+    // TODO: Your code here
+  };
+  
+
+  const myAge = function (age) {
+return myAge
+};
+  
+
+// Q4 )Write a function incrementOne that accepts one argument number and returns the number incremented by one when executed.
+
+
+const incrementOne = function (number) {
+  // TODO: Your code here
+};
+
+// an example of the output value when executing the function
+// add one to the the number that was passed in
+incrementOne(20); // => 21
+incrementOne(34); // => 35
+
+// Q5) Write a function greet that accepts one argument name and returns a string saying as shown below.
+
+const greet = function (moath) {
+    return greet
+  };
